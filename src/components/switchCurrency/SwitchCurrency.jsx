7924ad5ea027d1e7a34adb585f1dcfc6e60933df
Gamaunov <gamaunov1911@gmail.com>
2023-05-12 18:15:52 +0300

    fixed SwitchCurrency

--- src/components/switchCurrency/SwitchCurrency.jsx
+++ src/components/switchCurrency/SwitchCurrency.jsx
@@ -3,19 +3,23 @@ import { HiOutlineSwitchHorizontal } from 'react-icons/all'
 
 import s from './SwitchCurrency.module.scss'
 import { setFromValue, setToValue } from '../../redux/currencySlice.js'
+import { useState } from 'react'
 
 const SwitchCurrency = () => {
   const dispatch = useDispatch()
   const from = useSelector((state) => state.currency.from)
   const to = useSelector((state) => state.currency.to)
-
+  const [toggle, setToggle] = useState(false)
   const handleSwitch = () => {
     dispatch(setToValue(from))
     dispatch(setFromValue(to))
+    setToggle(current => !current)
   }
   return (
     <button className={s.switch} onClick={handleSwitch}>
-      <HiOutlineSwitchHorizontal />
+      <HiOutlineSwitchHorizontal
+        className={toggle ? s.active : ''} 
+      />
     </button>
   )
 }
